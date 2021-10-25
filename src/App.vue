5b6ac1d265ed6c79f0b5986ca5ed61d039a5130c
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-10-25 22:11:02 +0700

    add slick

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <slider />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import slider from "./components/slider.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    slider
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  font-family: system-ui;
 }
 </style>
---
+++ src/components/slider.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app">
+    <VueSlickCarousel
+      v-bind="settings"
+      v-if="slides.length"
+      ref="carousel"
+      class="carousel"
+    >
+      <div class="work" v-for="img in slides" :key="img">
+        <img :src="img" />
+      </div>
+    </VueSlickCarousel>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-unused-vars */
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+
+export default {
+  name: "WorksCarousel",
+  components: { VueSlickCarousel },
+  data() {
+    return {
+      slides: [
+        "https://loremflickr.com/320/240?random=1",
+        "https://loremflickr.com/320/240?random=2",
+        "https://loremflickr.com/480/480?random=3",
+        "https://loremflickr.com/320/240?random=4",
+        "https://loremflickr.com/320/240?random=5"
+      ],
+      currentSlide: 0,
+      settings: {
+        autoplay: true,
+        adaptiveHeight: true,
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        variableWidth: true,
+        draggable: true
+      }
+    };
+  },
+  watch: {
+    currentSlide(newValue, oldValue) {
+      this.$refs.carousel.goTo(newValue);
+    }
+  },
+  methods: {
+    updateIndexOnClick(slideIndex) {
+      this.currentSlide = slideIndex;
+    },
+    updateIndexOnDrag(oldSlideIndex, newSlideIndex) {
+      this.currentSlide = newSlideIndex;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.carousel {
+  width: 100%;
+  padding: 10px;
+}
+
+img {
+  user-select: none;
+  -webkit-user-drag: none;
+}
+
+.slick-slide {
+   max-height:200px;
+}
+
+.slick-slide img {
+   max-height:200px;
+   margin: 10px;
+}
+</style>
